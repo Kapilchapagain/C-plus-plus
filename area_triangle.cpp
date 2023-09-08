04994785b9b61e19741596deb6bdb3d68d0d449b
Kapilchapagain <125624289+kapil-2059@users.noreply.github.com>
2023-09-08 20:28:24 +0545

    Add files via upload

---
+++ area_triangle.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+class triangle
+{
+	public:
+		float base,height,area;
+	public:
+		void getdata()
+		{
+		cout<<"....Calculating area of a triangle..."<<endl;
+		cout<<"Enter the base: ";
+		cin>>base;
+		cout<<"Enter the height: ";
+		cin>>height;
+	}
+		void calculate_area()
+		{
+			area=(base*height)/2;
+			cout<<"The area of triangle is "<<area<<endl;
+		}
+	};
+int main()
+{
+	triangle area;
+	area.getdata();
+	area.calculate_area();
+	return 0;
+ 
+}
+
+
+
+
